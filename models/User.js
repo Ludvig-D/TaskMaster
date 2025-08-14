fd1249979c4103b7a99d61d0625208b8fa5bdb6b
Ludvig-D <74721111+Ludvig-D@users.noreply.github.com>
2025-08-14 22:20:25 +0200

    User gets pushed to db

--- models/User.js
+++ models/User.js
@@ -1,1 +1,16 @@
 import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 27,
+  },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.models.User || mongoose.model('User', UserSchema);
+export default User;
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import User from './models/User.js';
+
 const app = express();
 const PORT = 8080;
+
+mongoose.connect('mongodb://localhost:27017/TaskMaster');
+
+const user = await User.create({
+  username: 'testuser',
+  email: 'email@gmail.com',
+  password: 'test',
+});
+console.log(user);
 
 app.get('/', (req, res) => {
   res.send('hej');
