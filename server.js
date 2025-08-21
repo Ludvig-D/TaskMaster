e22fb336656b02db7bd29e77d58c300e7643f8b8
Ludvig-D <74721111+Ludvig-D@users.noreply.github.com>
2025-08-21 21:56:40 +0200

    Started the user creating process

--- server.js
+++ server.js
@@ -8,15 +8,32 @@ const PORT = 8080;
 
 mongoose.connect('mongodb://localhost:27017/TaskMaster');
 
-const user = await User.create({
-  username: 'testuser',
-  email: 'email@gmail.com',
-  password: 'test',
-});
-console.log(user);
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hej');
+});
+
+app.get('/login', (req, res) => {
+  res.send('login');
+});
+
+app.get('/signup', async (req, res) => {
+  res.send('signup');
+});
+
+app.post('/signup', async (req, res) => {
+  console.log(req.body);
+  try {
+    const { username, email, password } = req.body;
+    const existingEmail = await User.findOne({ email: email });
+    if (existingEmail) {
+      return res.status(400).send('Account with that email exists');
+    }
+    res.status(201).send('User registered successfully');
+  } catch (err) {
+    res.status(500).send('asda', err);
+  }
 });
 
 app.listen(PORT, () => {
