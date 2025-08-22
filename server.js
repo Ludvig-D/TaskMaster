81a8b386edfb98879d69a4cb526b3a1b711f7f75
Ludvig-D <74721111+Ludvig-D@users.noreply.github.com>
2025-08-22 13:17:24 +0200

    Creating users work now

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 import User from './models/User.js';
 
@@ -19,17 +20,29 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/signup', async (req, res) => {
+  const users = User.find();
+  console.log(users);
   res.send('signup');
 });
 
 app.post('/signup', async (req, res) => {
-  console.log(req.body);
   try {
     const { username, email, password } = req.body;
     const existingEmail = await User.findOne({ email: email });
     if (existingEmail) {
       return res.status(400).send('Account with that email exists');
     }
+
+    const hasedpassword = await bcrypt.hash(password, 16);
+
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: hasedpassword,
+    });
+
+    await newUser.save();
+
     res.status(201).send('User registered successfully');
   } catch (err) {
     res.status(500).send('asda', err);
